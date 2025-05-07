86bf19afb6a705f167e127faa668748d37ab539a
Martin Edlund <edlund@netzwerkplan.de>
2025-05-07 17:49:16 +0200

    upgrade to bevy 0.16

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,7 @@
-use bevy::prelude::{Handle, Shader};
+use bevy::{
+    asset::weak_handle,
+    prelude::{Handle, Shader},
+};
 
 pub mod material;
 pub mod mesh;
@@ -10,4 +13,4 @@ pub mod prelude {
     pub use crate::plugin::PointsPlugin;
 }
 
-pub const SHADER_HANDLE: Handle<Shader> = Handle::weak_from_u128(15532858032624716725);
+pub const SHADER_HANDLE: Handle<Shader> = weak_handle!("24afcc18-4e4b-47ea-9aee-c4c36bebd555");
